65a24f443ff9761ee0883da6176776aa51944057
ru10ben <rubenortizburgos@gmail.com>
2015-09-16 10:58:47 +0200

    MySQL
    Añadido conexión y módulo

--- projectTFG/app.js
+++ projectTFG/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mysql = require('mysql');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -26,6 +27,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+// DATABASE
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    port     : 3306,
+    database : 'myapp',
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
