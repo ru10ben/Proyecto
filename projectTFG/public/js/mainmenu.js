a5fa7b17f8aa24df29dfda2900286a81ec7ef566
juanysho <juanmonterocallealta@gmail.com>
2015-09-26 12:53:08 +0200

    Login Final y Otros cambios

--- projectTFG/public/js/mainmenu.js
+++ projectTFG/public/js/mainmenu.js
@@ -1,4 +1,16 @@
 $(function(){
+    flag=false;
+    $('#admin').click(function(){
+        $('#admin').text('Hide Administrator actions');
+        flag=true;
+        if(flag == true){
+            console.log('algo')
+            $('#admin').click(function(){
+                $('#admin').text('Show Administrator actions');
+            })
+        }  
+  })
+    
 	$('#h5name').text(name);
 
 });
