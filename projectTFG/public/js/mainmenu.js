637f70c500baa19eb9b26ed119d5b4b3c588ddaa
ru10ben <rubenortizburgos@gmail.com>
2015-09-30 13:54:31 +0200

    Mainmenu
    Acordeon respecto a los roles

--- projectTFG/public/js/mainmenu.js
+++ projectTFG/public/js/mainmenu.js
@@ -3,17 +3,26 @@ $(function(){
     var role=$.cookie('roles');
     $('#username').text('User name: '+username);
     $('#role').text('Role: '+role);
+
+    if(role.indexOf('Evaluator')>-1){
+       $('#actions3').show(); 
+    }
+    if(role.indexOf('Administrator')>-1){
+       $('#actions').show(); 
+    }
+    if(role.indexOf('Project Manager')>-1){
+       $('#actions2').show(); 
+    }
+
     $('#admin').click(function(){
         $('#admin').text('Hide Administrator actions');
-        flag=true;
-        if(flag == true){
-            console.log('algo')
+        if(role == "Administrator"){
+            console.log('algo');
             $('#admin').click(function(){
+                location.reload();
                 $('#admin').text('Show Administrator actions');
             })
         }  
   })
     
-	$('#h5name').text(name);
-
 });
