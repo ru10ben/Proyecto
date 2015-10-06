b9b463ca392ad8083ce64eb93b43d78fb5fc5e83
juanysho <juanmonterocallealta@gmail.com>
2015-10-06 15:47:47 +0200

    Index.js

--- projectTFG/routes/index.js
+++ projectTFG/routes/index.js
@@ -4,31 +4,30 @@ var user;
 var id_user=1;
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    console.log('paso 1');
   res.render('login', { title: 'Express' });
 });
 
-router.post('/login', function(req, res, next) {
-  //res.render('login', { title: 'Express' });
-  var username=req.body.username;
-  var password=req.body.password;
-
-  getUser(username, function(err, result){
-    if (result.length == 0 || password != result[0].password) {
-      res.send('ERROR');
-    }else{
-      user={username: result[0].userName, roles:result[0].roles}
-      res.send(user);
-    }
-  });
+router.post('/log', function(req, res, next) {
+    console.log('paso 2');
+    var username=req.body.username;
+    var password=req.body.password;
+    console.log(username); 
+    console.log(password);
+    
+    getUser(username, function(err, result){
+        if (result.length == 0 || password != result[0].password) {
+            res.send('ERROR');
+        }else{
+            user={username: result[0].userName, roles:result[0].roles}
+            res.send(user);
+        }
+    });
 });
 
 router.get('/mainMenu', function(req, res, next) {
-   console.log(user.username);
-   res.render('mainMenu');
-});
-
-router.get('/newUser', function(req, res, next) {
-  res.render('newUser');
+    console.log('paso 3');
+  res.render('mainMenu');
 });
 
 router.post('/insertUser', function(req, res, next) {
@@ -39,17 +38,16 @@ router.post('/insertUser', function(req, res, next) {
   var password=req.body.password;
   var email=req.body.email;
   var roles=req.body.role;
-  console.log('booooody: '+firstname);
-  console.log('Roles: '+roles);
   var comments=req.body.comments;
-
+    //-----------------------------------------------------------
+    console.log(email); //----------------------------------
+    console.log(firstname);
+//----------------------------------------------------------------------
   getUser(username, function(err, result){
-  	console.log(result);
     if (result.length != 0) {
-    	res.send('ERROR');
+      res.send('ERROR');
     }else{
-      maxIdUser(function(err, rows){
-      	 console.log(rows);
+    maxIdUser(function(err, rows){
          var id_string = JSON.stringify(rows);
          id_string = id_string.replace('max(id)','');
          id_string = id_string.replace('"','');
@@ -62,7 +60,6 @@ router.post('/insertUser', function(req, res, next) {
         }else{
           id_user=1;
         }
-        console.log(id_user);
         insertUser(id_user, firstname, lastname, username, password, email, roles, comments, function(err, result){
             id_user=id_user+1;
             res.send('OK');
@@ -70,6 +67,11 @@ router.post('/insertUser', function(req, res, next) {
       });
     }
   });
+});
+
+router.get('/newUser', function(req, res, next) {
+    console.log('paso 4');
+  res.render('newUser');
 });
 
 module.exports = router;
