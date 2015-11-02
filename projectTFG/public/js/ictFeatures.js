967ba26818bf8df342857d269098b171ce386d72
juanysho <juanmonterocallealta@gmail.com>
2015-11-02 19:56:29 +0100

    Mas cambio cliente

--- projectTFG/public/js/ictFeatures.js
+++ projectTFG/public/js/ictFeatures.js
@@ -11,7 +11,7 @@ $(function(){
     $('#question').text(question);
     $('#help').text(help);
     select1 = document.getElementById("listclauses");
-    
+    select = document.getElementById("listhistoric");
     var i=0;
     while(i<clausesArr.length){
 
@@ -24,18 +24,33 @@ $(function(){
     $("#nextquest").on("click",function(){
         var param = { answer:$("input[name='answer']:checked").val()};
         $.post( "/next", param, function( data ) {
+            if(data!='You have completed the evaluation'){
             $('#question').text(data.question);
             $('#help').text(data.help);
             var clausesArr2=data.clauses;
-            //console.log(clausesArr2)
+            var historic=data.historic;
+            var actualHistoric=historic[historic.length-1];
+                
+                //if(actualHistoric.length>8){
+                //    var parte=actualHistoric.substring(2);
+                    
+            //    }
+            
+            var opt2 = document.createElement("option");
+               // if(actualHistoric.length>8){
+                //    opt2.innerHTML = actualHistoric.join("\n");
+                //}
+            opt2.innerHTML = actualHistoric;
+            select.appendChild(opt2);
+            
             while(i<clausesArr2.length){
-
                 var opt = document.createElement("option");
                 opt.value = i;
                 opt.innerHTML = clausesArr2[i];
                 select1.appendChild(opt);
                 i++;
-            } 
+            }
+            }else{console.log(data);}
         });
     });
 });
