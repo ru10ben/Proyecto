21cbfe38a03eccccf43de34360ec74382474e2b4
ru10ben <rubenortizburgos@gmail.com>
2015-11-10 14:57:29 +0100

    Cliente

--- projectTFG/public/js/assingEvaluator.js
+++ projectTFG/public/js/assingEvaluator.js
@@ -5,9 +5,11 @@ $(function(){
     var i=0;
     var valor;
     var texto;
+    var valor2;
+    var texto2;
     select = document.getElementById("evaluatorName1");
     select2 = document.getElementById("evaluatorName2");
-    var opt2 = document.createElement("option");
+    //var opt2 = document.createElement("option");
 
     while(i<listArray.length){
         var opt = document.createElement("option");
@@ -17,21 +19,38 @@ $(function(){
         i++;
     } 
     
-$('select#evaluatorName1').on('change',function(){
-    valor = $(this).val();
-    texto = $("#evaluatorName1 option:selected").text();
-    console.log(valor);
-    console.log(texto);
-});
+    $('select#evaluatorName1').on('change',function(){
+        valor = $(this).val();
+        texto = $("#evaluatorName1 option:selected").text();
+        console.log(valor);
+        console.log(texto);
+    });
     
+    $('select#evaluatorName2').on('change',function(){
+        valor2 = $(this).val();
+        texto2 = $("#evaluatorName2 option:selected").text();
+        console.log(valor2);
+        console.log(texto2);
+    });
 
     
     $('#right').click(function(){ //en proyecto
         //$("<option value='5'>Scientific Linux</option>").appendTo("#miselect");
+        var opt2 = document.createElement("option");
         opt2.value=valor;
         opt2.innerHTML = texto;
         select2.appendChild(opt2);
-        $("#evaluatorName1 option[value='valor']").remove();
+        $("#evaluatorName1 option[value='"+valor+"']").remove();
         
-    })
+    });
+
+    $('#left').click(function(){ //en proyecto
+        //$("<option value='5'>Scientific Linux</option>").appendTo("#miselect");
+        var opt3 = document.createElement("option");
+        opt3.value=valor2;
+        opt3.innerHTML = texto2;
+        select.appendChild(opt3);
+        $("#evaluatorName2 option[value='"+valor+"']").remove();
+        
+    });
 });
